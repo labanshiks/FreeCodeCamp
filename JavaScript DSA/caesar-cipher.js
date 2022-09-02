8abf9894f080eb507237321279b92e90b1f78bba
labanshiks <labanshiks4@gmail.com>
2022-09-02 17:36:16 +0300

    caesar-cipher

---
+++ JavaScript DSA/caesar-cipher.js
@@ -0,0 +1,19 @@
+const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+function rot13(str) {
+    let accumulator = '';
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        const isLetter = alphabet.includes(char);
+        if (isLetter === false) {
+            accumulator += char;
+        } else {
+            const charIndex = alphabet.findIndex((c) => c === char);
+            accumulator += alphabet[charIndex + 13] || alphabet[charIndex - 13];
+        }
+    }
+    return accumulator;
+}
+
+console.log(rot13("SERR PBQR PNZC"));
